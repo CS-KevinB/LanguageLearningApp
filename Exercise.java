4a2f735862459217f0d52dbac6a7dc837d3561f0
Kevin Buie <buiek@email.sc.edu>
2024-10-06 11:16:43 -0400

    Added javadoc

--- Exercise.java
+++ Exercise.java
@@ -9,6 +9,12 @@ import java.util.ArrayList;
 public class Exercise {
     private ArrayList<Question> questions;
 
+    /**
+     * Creates a exercise based on if exercise is empty
+     * @param exercise needs exercise in order to check if there is an array list of question or not
+     * @param question takes in a question
+     * CHECK THIS
+     */
     public Exercise(Exercise exercise, Question question){
         if(exercise != null){
             this.questions = new ArrayList<>(exercise.getQuestion());
@@ -17,6 +23,10 @@ public class Exercise {
         }
     }
 
+    /**
+     * Gets the array list of questions for the user
+     * @return returns this array list in order to create an exercise
+     */
     public ArrayList<Question> getQuestions(){
         return questions;
     }

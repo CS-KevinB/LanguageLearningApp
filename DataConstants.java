eb3e0e919e31042ae8229e0d59255a2d7d2f9ffd
Kinsey Williams <161615678+kinsawills@users.noreply.github.com>
2024-10-06 12:06:48 -0400

    fixed #90

---
+++ DataConstants.java
@@ -0,0 +1,21 @@
+public abstract class DataConstants {
+    protected static final String USER_FILE_NAME = "src/users.json";
+    protected static final String USER_ID = "userid";
+    protected static final String USER_FIRST_NAME = "firstName";
+    protected static final String USER_LAST_NAME = "lastName";
+    protected static final String USER_USERNAME = "username";
+    protected static final String USER_EMAIL = "email";
+    protected static final String USER_BIRTHDAY = "birthday";
+    protected static final String USER_AVATAR = "avatar";
+    protected static final String CHARACTER = "character";
+    protected static final String HAT = "hat";
+    protected static final String AVATAR_IMAGE_PATH = "avatarImagePath";
+    protected static final String LANGUAGE_ID = "lanugageid";
+    protected static final String FRIENDS_ID = "friendsid";
+    protected static final String USER_POINTS = "points";
+    protected static final String USER_PROGRESS = "userProgress";
+    protected static final String CURRENT_LESSON = "currentLesson";
+    protected static final String CURRENT_EXERCISE = "currentExercise";
+
+    
+}

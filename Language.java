8ff6855d89f407d8880e64064df9fb36b929909e
Kinsey Williams <161615678+kinsawills@users.noreply.github.com>
2024-10-06 11:33:12 -0400

    fixed #41

--- Language.java
+++ Language.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class Language {
-    
+    private UUID id;
+    private String nameOfLanguage;
+    private Arraylist<Lesson> lessons;
+    private ArrayList<Word> words;
+    private ArrayList<Phrase> phrases;
+
+    public Language(String nameOfLanguage, ArrayList<Lesson> lessons, ArrayList<Word> words, ArrayList<Phrase> phrases) {
+
+    }
+
+    public ArrayList<Lesson> getLessons() {
+        
+    }
 }

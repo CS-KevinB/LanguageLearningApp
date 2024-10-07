41044a7f0545e3e518a5ed08ed7e9ff46cb632e5
Risha Patel <rishap@email.sc.edu>
2024-10-06 23:01:59 -0400

    Added javaDocs for #33

--- Story.java
+++ Story.java
@@ -10,17 +10,29 @@ public class Story {
     private ArrayList<Word> englishStory;
     private ArrayList<Word> spanishStory; 
 
+    /**
+     * Constructor for story class
+     * @param title - title of the story
+     * @param englishStory - list of words for the english story
+     * @param spanishStory - list of words for the spanish story
+     */
     public Story(String title){
         this.title = title;
         this.englishStory = new ArrayList<>();
         this.spanishStory = new ArrayList<>();
     }
-
+    /**
+     * Removes all the words from the story
+    * @return - void
+    */
     public void remvoveWords(){
         englishStory.clear();
         spanishStory.clear();
     }
-
+    /**
+     * To check if the story is correct
+     * @return boolean
+     */
     public boolean isCorrect(String story){
         return this.title.equalsIgnoreCase(story);
     }

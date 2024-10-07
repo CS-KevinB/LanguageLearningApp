f06b153ccfc7fec9d1a7c956d0c1dbc460c3df22
Risha Patel <rishap@email.sc.edu>
2024-10-06 23:31:29 -0400

    Fixes for #96

--- Main.java
+++ Main.java
@@ -3,10 +3,8 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
         ArrayList<User> users = DataLoader.getUsers();
-        DataWriter.saveUsers(users);    
-        
+        DataWriter.saveUsers(users);       
     }
 
 }

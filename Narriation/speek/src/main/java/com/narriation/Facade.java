79063833e0fe073f2c7e8a5fea7aee4e160ee337
christianruff <36940386+christianruff@users.noreply.github.com>
2024-10-09 13:21:43 -0400

    to get rid of red squiggles

--- Narriation/speek/src/main/java/com/narriation/Facade.java
+++ Narriation/speek/src/main/java/com/narriation/Facade.java
@@ -11,43 +11,44 @@ public class Facade {
     }
 
     public User login(String username, String password) {
-        
+        return null;
     }
 
     public User createUser() {
-
+        return null;
     }
 
     public boolean logout() {
-
+        return false;
     }
 
-    public void updateAvatar(CharacterOption character, HatOption hat) {
-
+    public void updateAvatar(CharacterOptions character, HatOptions hat) {
+        
     }
 
     public boolean updateUser(String firstname, String lastname, String emailAddress) {
-
+        return false;
     }
 
     public boolean updateCredentials(String username, String password) {
-
+        return false;
     }
 
     public boolean startLesson() {
-
+        return false;
     }
 
     public boolean startExercise() {
+        return false;
 
     }
 
     public boolean startStory() {
-
+        return false;
     }
 
-    public boolean setCurrentLangauge(Langauage language) {
-
+    public boolean setCurrentLangauge(Language language) {
+        return false;
     }
 
     public void sendNotification(NotificationType notificationType) {

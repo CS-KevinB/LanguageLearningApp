e9257817674ef3086f177a33e04883a60ccc94f9
christianruff <36940386+christianruff@users.noreply.github.com>
2024-10-09 12:40:20 -0400

    Closes #110; Updated DataLoader for UserProgress #116

--- Narriation/speek/src/main/java/com/narriation/UserProgress.java
+++ Narriation/speek/src/main/java/com/narriation/UserProgress.java
@@ -4,17 +4,33 @@ package com.narriation;
  * @author christianruff
  */
 public class UserProgress {
-    private Lesson currentLesson;
-    private Exercise currentExercise;
+    private int currentLesson;
+    private int currentExercise;
 
     /**
      * Constructs a new UserProgress object
      */
     public UserProgress() {
-        this.currentLesson = null;
-        this.currentExercise = null;
+        this.currentLesson = 1;
+        this.currentExercise = 1;
     }
     
+    /**
+     * Constructs a new UserProgress object
+     * @param currentLesson Position of current lesson in array
+     * @param currentExercise Position of current exercise in array
+     */
+    public UserProgress(int currentLesson, int currentExercise) {
+        if (currentLesson > 0)
+            this.currentLesson = currentLesson;
+        else
+            this.currentLesson = 1;
+        if (currentExercise > 0)
+            this.currentExercise = currentExercise;
+        else
+            this.currentExercise = 1;
+    }
+
     /**
      * Returns if there is a next exercise
      * @return TRUE if there is a next exercise, FALSE otherwise
@@ -45,5 +61,10 @@ public class UserProgress {
      */
     public Exercise nextExercise() {
         return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Lesson = " + this.currentLesson + " | Exercise = " + this.currentExercise;
     }
 }

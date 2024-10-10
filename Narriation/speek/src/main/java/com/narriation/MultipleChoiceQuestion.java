e642e595894203b8b0ac0451c2f7d6f2c7a81c55
Kevin Buie <buiek@email.sc.edu>
2024-10-10 13:17:42 -0400

    Changes

--- Narriation/speek/src/main/java/com/narriation/MultipleChoiceQuestion.java
+++ Narriation/speek/src/main/java/com/narriation/MultipleChoiceQuestion.java
@@ -5,14 +5,14 @@ package com.narriation;
  */
 import java.util.UUID;
 import java.util.ArrayList;
-public class MultipleChoice implements Question{
+public class MultipleChoiceQuestion implements Question{
     
 
     private ArrayList<Word> words;
     private Word correctAnswer;
     private UUID id;
 
-    public MultipleChoice(ArrayList<Word> words, Word correctAnswer, UUID id){
+    public MultipleChoiceQuestion(ArrayList<Word> words, Word correctAnswer, UUID id){
         this.words = words;
         this.correctAnswer = correctAnswer;
         this.id = id;

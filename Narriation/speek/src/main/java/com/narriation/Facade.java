e72913fcae5b848985e7fc81565b1a051ff34cfd
Kinsey Williams <161615678+kinsawills@users.noreply.github.com>
2024-10-24 13:48:47 -0400

    Add getCurrentStory method

--- Narriation/speek/src/main/java/com/narriation/Facade.java
+++ Narriation/speek/src/main/java/com/narriation/Facade.java
@@ -179,8 +179,8 @@ public class Facade {
 
     // }
 
-    public void viewLanguage() {
-        languages.getLanguageByUUID(currentLanguage.getUUID());
+    public Language getCurrentLanguage() {
+        return languages.getLanguageByUUID(currentLanguage.getUUID());
     }
 
     public void startLesson() {

126bdbc6e7286775416f93e9caa0f43c8889f30e
Risha Patel <rishap@email.sc.edu>
2024-10-25 12:21:02 -0400

    Adding method to convert user object to Json object

--- Narriation/speek/src/main/java/com/narriation/DataWriter.java
+++ Narriation/speek/src/main/java/com/narriation/DataWriter.java
@@ -14,9 +14,11 @@ import org.json.simple.JSONObject;
 
 public class DataWriter extends DataConstants {
 
+    private static final Object USER_PROGRESS = null;
 
     /**
      * Saves the users to a json file
+     * 
      * @param users requires the list of users in order to save
      * @return returns a boolean if the save was successful
      */
@@ -45,22 +47,38 @@ public class DataWriter extends DataConstants {
 
     /**
      * Saves the user to a json file
+     * 
      * @param user requires the specific user now in order to add to the json file
      * @return returns a JSON Object
      */
+    @SuppressWarnings("unchecked")
     public static JSONObject getUserJSON(User user) {
         JSONObject userDetails = new JSONObject();
-        userDetails.put(USER_ID, user.getUsername().toString());
+
+        userDetails.put(USER_ID, user.getUUID().toString());
         userDetails.put(USER_FIRST_NAME, user.getFirstName());
         userDetails.put(USER_LAST_NAME, user.getLastName());
+        userDetails.put(USER_USERNAME, user.getUsername());
         userDetails.put(USER_PASSWORD, user.getPassword());
         userDetails.put(USER_EMAIL, user.getEmailAddress());
-        userDetails.put(USER_BIRTHDAY, user.getBirthday());
-        userDetails.put(USER_AVATAR, user.getAvatar().getImagePath());
+        userDetails.put(USER_BIRTHDAY, user.getBirthday().toString());
         userDetails.put(USER_POINTS, user.getPoints());
-        // userDetails.put(USER_PROGRESS, user.getUserProgress().getProgress());
-        userDetails.put(FRIENDS, user.getFriends());
-        return userDetails;
+
+        // Avatar information
+        JSONObject avatarJSON = new JSONObject();
+        avatarJSON.put(CHARACTER, user.getAvatar().getCharacter());
+        avatarJSON.put(HAT, user.getAvatar().getHat());
+        userDetails.put(USER_AVATAR, avatarJSON);
+
+        // Friends list
+        JSONArray friendsArray = new JSONArray();
+        for (User friend : user.getFriends()) {
+            friendsArray.add(friend.getUUID().toString());
+        }
+        userDetails.put(FRIENDS, friendsArray);
+
+        // User Progress
+
     }
 
     public static void main(String[] args) {

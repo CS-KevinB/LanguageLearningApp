5a02457ce3953f1d08a428cc3401c57c59557eab
Risha Patel <rishap@email.sc.edu>
2024-11-03 18:24:55 -0500

    Creating testAddUser for UserList

--- Narriation/speek/src/test/java/com/narriation/library/UserListTest.java
+++ Narriation/speek/src/test/java/com/narriation/library/UserListTest.java
@@ -4,13 +4,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.UUID;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.narriation.Avatar;
@@ -23,20 +21,28 @@ import com.narriation.UserProgress;
 public class UserListTest {
     private UserList userList;
     private ArrayList<User> users;
-    private User user;
-    private User friend;
+    private User user1;
+    private User user2;
+    private User user3;
 
     @Before
     public void setup() {
 
         userList = UserList.getInstance();
-        users = userList.getUsers();
-        users.clear();
-        user = new User(UUID.randomUUID(), "John", "Doe", "jDoe", "do", "jdoe@gmail.com",
-                new Date(), new Avatar("CharacterA", "HatA"), 0, new ArrayList<UserProgress>());
+        userList.getUsers().clear();
 
-        friend = new User(UUID.randomUUID(), "Jane", "Smith", "jSmith", "smith", "jsmith@gmail.com",
-                new Date(), new Avatar("CharacterB", "HatB"), 0, new ArrayList<UserProgress>());
+        user1 = new User(UUID.randomUUID(), "John", "Doe", "jDoe", "doe", "jdoe@gmail.com",
+                new Date(), new Avatar(), 0, new ArrayList<UserProgress>());
+
+        user2 = new User(UUID.randomUUID(), "Jane", "Smith", "jSmith", "smith", "jsmith@gmail.com",
+                new Date(), new Avatar(), 0, new ArrayList<UserProgress>());
+
+        user3 = new User(UUID.randomUUID(), "Jane", "Doe", "jhoneD", "123", "jane@gmail.com",
+                new Date(), new Avatar(), 0, new ArrayList<UserProgress>());
+
+        userList.addUser(user1);
+        userList.addUser(user2);
+        // userList.addUser(user3);
     }
 
     @Test
@@ -46,9 +52,10 @@ public class UserListTest {
 
     @Test
     public void testAddUser() {
-
-        userList.addUser(user);
-        assertTrue(users.contains(user));
+        assertEquals(2, userList.getUsers().size());
+        userList.addUser(user3);
+        assertEquals(3, userList.getUsers().size());
+        assertTrue(userList.getUsers().contains(user3));
     }
 
 }
